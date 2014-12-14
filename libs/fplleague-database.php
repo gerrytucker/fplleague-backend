4fd57fc2a35fd7365bf887d4996eb7c681adf81d
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-14 18:01:51 +0000

    Auto-generated commit 14/12/2014 6:01:44 PM

--- libs/fplleague-database.php
+++ libs/fplleague-database.php
@@ -1190,10 +1190,10 @@ if ( ! class_exists( 'FPLLeague_Database' ) ) {
 			return $wpdb->get_results( $wpdb->prepare(
 				"SELECT d.id, p1.id_player_1, p1.name_player_1, p2.id_player_2, p2.name_player_2, d.id_team, t.name as team_name
 				FROM $wpdb->doubles AS d
-				LEFT JOIN $wpdb->players AS p1
-				ON p.id_player_1 = d.id_player_1
-				LEFT JOIN $wpdb->players AS p2
-				ON p.id_player_2 = d.id_player_2
+				LEFT JOIN $wpdb->players p1
+				ON p1.id = d.id_player_1
+				LEFT JOIN $wpdb->players p2
+				ON p2.id = d.id_player_2
 				LEFT JOIN $wpdb->teams AS t
 				ON p.id_team = t.id
 				ORDER BY t.name ASC, p1.name_player_1
