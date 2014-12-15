3158beed0bf11a95bcc566aae7fc2e895ac874ac
gerrytucker <gerrytucker@gerrytucker.co.uk>
2014-12-15 16:07:31 +0000

    Auto-generated commit 15/12/2014 4:07:23 PM

---
+++ incs/admin/add-doubles.php
@@ -0,0 +1,74 @@
+<?php
+$db = new FPLLeague_Database;
+
+if( isset( $_POST['publish'] ) ) {
+
+	$id_player_1 = $_POST['id_player_1'];
+	$id_player_2 = $_POST['id_player_2'];
+	$id_team = $_POST['id_team'];
+
+	if ( $db->add_doubles( $id_player_1, $id_player_2, $id_team ) ) {
+
+		wp_redirect( admin_url( 'admin.php?page=fplleague-doubles&message=150' ) );
+		exit;
+
+	} else {
+
+	}
+
+}
+
+if ( isset( $_POST['noheader'] ) ) {
+	require_once ABSPATH . 'wp-admin/admin-header.php';
+}
+
+?>
+
+<div class="wrap">
+
+	<h2>Add New Doubles</h2>
+
+	<form name="post" id="post" action="<?php echo admin_url( 'admin.php?page=' . $_REQUEST['page'] . '&noheader=true' ); ?>" method="post">
+
+		<table class="form-table">
+
+			<tbody>
+
+				<tr>
+
+					<th scope="row">
+
+						<label for="id_team">Step 1 - Select A Team</label>
+
+					</th>
+
+					<td>
+
+						<select name="id_team">
+
+							<option value="">Select A Team...</option>
+
+							<?php
+							$team = $db->get_every_team( NULL, 0, 9999, 'ASC' );
+							foreach ( $team as $team ) :
+							?>
+
+							<option value="<?php echo $team['id']; ?>"><?php echo $team['name']; ?></option>
+
+							<?php endforeach; ?>
+
+						</select>
+
+					</td>
+
+				</tr>
+
+			</tbody>
+
+		</table>
+
+		<input class="button button-primary button-large" type="submit" name="publish" id="publish" value="<?php _e( 'Save' ); ?> disabled">
+
+	</form>
+
+</div>
